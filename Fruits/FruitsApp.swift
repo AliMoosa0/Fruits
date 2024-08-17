c1d63c96f73960893b3b998bce8fae99edf824fb
ali Moosa <alm9ly>
2024-08-17 21:26:24 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by ALI MOOSA on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
